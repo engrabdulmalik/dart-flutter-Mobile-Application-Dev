12fd28eae763f5f2fcdef1435aa43ba28302e129
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-09-24 10:16:54 +0500

    lab

--- lab_1_24_09_2024/bin/lab_1_24_09_2024.dart
+++ lab_1_24_09_2024/bin/lab_1_24_09_2024.dart
@@ -1,3 +1,12 @@
 void main() {
+  //var , const, final
+
+  String variable="Hello";
+
+  print(variable?.length??1);
+
+// doubleVariable = 20.5;
+// print(doubleVariable);
+
   print('First Lab of Dart and Flutter Installation');
 }
