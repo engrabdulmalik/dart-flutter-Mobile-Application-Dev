3c83ab571f15e84ad0f00f30177a3b6f96e75d86
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-10-26 08:53:10 +0500

    changed

--- lab_1_24_09_2024/bin/class_2.dart
+++ lab_1_24_09_2024/bin/class_2.dart
@@ -1,7 +1,30 @@
 void main() {
-
 //Condiational Statements
 
+// Single Alternative
 
+  var marks = "eighty";
 
+//   if (marks <= 40) {
+//     print("Pass");
+//   } else if (marks == 20) {
+//     print("fail");
+//   } else if (marks >= 80) {
+//     print("You passed with \n flying colors");
+//   } else {
+//     print("You failed");
+//   }
+
+// print(marks > 50 ? "pass" : "fail");
+
+  switch (marks) {
+    case "eighty" when marks.length <= 40:
+
+      print("You passed with flying colors");
+   
+    case 0:
+      print("You failed");
+    default:
+      print("Incomplete Grade");
+  }
 }
